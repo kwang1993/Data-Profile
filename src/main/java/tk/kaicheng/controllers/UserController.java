d65826c824eb4a4b4ada9fb610b5be52f872720c
ACAI-PC <hcheng1990@gmail.com>
2017-08-25 22:29:42 -0700

    Added index page, link to register/login

--- src/main/java/tk/kaicheng/controllers/UserController.java
+++ src/main/java/tk/kaicheng/controllers/UserController.java
@@ -28,7 +28,12 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value={"/", "/login"}, method = RequestMethod.GET)
+    @RequestMapping(value={"/"}, method = RequestMethod.GET)
+    public String home(){
+        return "index";
+    }
+
+    @RequestMapping(value={"/login"}, method = RequestMethod.GET)
     public String login(){
         return "login";
     }
