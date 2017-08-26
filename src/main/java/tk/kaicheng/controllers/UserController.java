77fc4f49e5f953e4a1a6570833dbdbef8f0579a4
ACAI-PC <hcheng1990@gmail.com>
2017-08-26 15:08:38 -0700

    remove index page
    don't know how to set default page to index

--- src/main/java/tk/kaicheng/controllers/UserController.java
+++ src/main/java/tk/kaicheng/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -29,17 +30,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value={"/"}, method = RequestMethod.GET)
-    public String home(){
-        if(isLoggedIn()){
-            System.out.println("hello guest");
-        } else {
-            System.out.println(getLoggedUser());
-        }
-        return "index";
-    }
-
-    @RequestMapping(value={"/login"}, method = RequestMethod.GET)
+    @RequestMapping(value={"/","/login"}, method = RequestMethod.GET)
     public String login(){
         if(isLoggedIn()) {
             return "login";
